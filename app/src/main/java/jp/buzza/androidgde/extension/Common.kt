21c2fcbb733df670beb56aedd86aaa7e268de171
vegeta2102 <hoangvmh@gmail.com>
2020-04-18 18:33:30 +0900

    Add get or null from bundle

--- app/src/main/java/jp/buzza/androidgde/extension/Common.kt
+++ app/src/main/java/jp/buzza/androidgde/extension/Common.kt
@@ -3,7 +3,11 @@ package jp.buzza.androidgde.extension
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 
+/**
+ * This type of function is not good
+ */
 fun <T : Activity> Activity.startActivityWithoutReified(
     context: Context,
     className: Class<T>
@@ -11,8 +15,18 @@ fun <T : Activity> Activity.startActivityWithoutReified(
     startActivity(Intent(context, className))
 }
 
+/**
+ * Using inline vs reified is the best choice
+ */
 inline fun <reified T : Activity> Activity.startActivity(
     context: Context
 ) {
     startActivity(Intent(context, T::class.java))
+}
+
+/*
+ * Safe way to get return value
+ */
+inline fun <reified T> Bundle.getDataOrNull(key: String): T? {
+    return getSerializable(key) as? T
 }
